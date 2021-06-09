f476c7cfb127fd4162179ecaddafe5f24f4be302
wilmerzuna <zunalargowilmer@gmail.com>
2021-06-09 13:59:15 -0400

    Implementing partially working meny icon feature in topbar

--- client/src/components/TopBar/TopBar.jsx
+++ client/src/components/TopBar/TopBar.jsx
@@ -8,16 +8,23 @@ import './TopBar.css';
 class TopBar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {menu: null};
   }
 
   render() {
     return (
       <AppBar position='absolute' style={this.props.style}>
         <Toolbar style={{minHeight:'50px'}}>
-          <IconButton>
-            <Menu color='black' style={this.props.style}>
-              <MenuItem>Profile</MenuItem>
+          <IconButton onClick={(e) => this.setState({ menu: e.currentTarget })}>
+            <Menu 
+              style={this.props.style} open={Boolean(this.state.menu)}
+              anchorEl={this.state.menu} onClose={() => this.setState({ menu: null })}
+            >
+              <MenuItem
+                onClose={() => {this.setState({ menu: null })}}
+              >
+                Profile
+              </MenuItem>
             </Menu>
           </IconButton>
           <Typography variant='h6'>Plem</Typography>
